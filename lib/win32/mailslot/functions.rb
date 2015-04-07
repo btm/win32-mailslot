626c349db2067653e6a4a2598f0833737c7792d9
Daniel Berger <djberg96@gmail.com>
2015-04-07 13:33:12 -0600

    Updated functions.

--- lib/win32/mailslot/functions.rb
+++ lib/win32/mailslot/functions.rb
@@ -1,7 +1,7 @@
 require 'ffi'
 
-module Win32
-  class Mailslot
+module Windows
+  module Mailslot
     module Functions
       extend FFI::Library
 
@@ -11,7 +11,7 @@ module Win32
       typedef :ulong, :dword
 
       attach_function :CloseHandle, [:handle], :bool
-      attach_function :CreateMailslotA, [:string, :dword, :dword, :pointer], :handle
+      attach_function :CreateMailslot, :CreateMailslotA, [:string, :dword, :dword, :pointer], :handle
       attach_function :GetMailslotInfo, [:handle, :pointer, :pointer, :pointer, :pointer], :bool
       attach_function :SetMailslotInfo, [:handle, :dword], :bool
     end
