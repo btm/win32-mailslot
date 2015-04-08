2f1eaea806fd19d94c24e0e70ba2529bf672f1e0
Daniel Berger <djberg96@gmail.com>
2015-04-08 10:31:27 -0600

    Added Readfile and Writefile.

--- lib/win32/mailslot/functions.rb
+++ lib/win32/mailslot/functions.rb
@@ -9,11 +9,14 @@ module Windows
 
       typedef :uintptr_t, :handle
       typedef :ulong, :dword
+      typedef :pointer, :ptr
 
       attach_function :CloseHandle, [:handle], :bool
-      attach_function :CreateMailslot, :CreateMailslotA, [:string, :dword, :dword, :pointer], :handle
-      attach_function :GetMailslotInfo, [:handle, :pointer, :pointer, :pointer, :pointer], :bool
+      attach_function :CreateMailslot, :CreateMailslotA, [:string, :dword, :dword, :ptr], :handle
+      attach_function :GetMailslotInfo, [:handle, :ptr, :ptr, :ptr, :ptr], :bool
+      attach_function :ReadFile, [:handle, :buffer_out, :dword, :ptr, :ptr], :bool
       attach_function :SetMailslotInfo, [:handle, :dword], :bool
+      attach_function :WriteFile, [:handle, :buffer_in, :dword, :ptr, :ptr], :bool
     end
   end
 end
